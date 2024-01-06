48718782336e15bf941faaacade9d21863033293
Dzreke-Poku Nathan <dzrekenathan2002@gmail.com>
2024-01-06 03:01:12 +0000

    Class Created

---
+++ app/src/main/java/com/tech/nate/gamification/kys/KnowYourScience.java
@@ -0,0 +1,16 @@
+package com.tech.nate.gamification.kys;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.tech.nate.gamification.R;
+
+public class KnowYourScience extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_know_your_science);
+    }
+}
