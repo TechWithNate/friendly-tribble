c6c9f7f6a28c3f34c5e68a9916979ab92e1343c4
Dzreke-Poku Nathan <dzrekenathan2002@gmail.com>
2024-01-06 02:55:24 +0000

    Class Created

---
+++ app/src/main/java/com/tech/nate/gamification/quizzes/brainteaser/BrainTeaser.java
@@ -0,0 +1,16 @@
+package com.tech.nate.gamification.quizzes.brainteaser;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.tech.nate.gamification.R;
+
+public class BrainTeaser extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_brain_teaser);
+    }
+}
