eb2046190d57128163bcbe215e477c37351eb4a2
Dzreke-Poku Nathan <dzrekenathan2002@gmail.com>
2024-01-12 18:19:16 +0000

    Home Screen Updated

--- app/src/main/java/com/tech/nate/gamification/HomeScreen.java
+++ app/src/main/java/com/tech/nate/gamification/HomeScreen.java
@@ -4,11 +4,32 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.google.android.material.card.MaterialCardView;
+
 public class HomeScreen extends AppCompatActivity {
+
+    private MaterialCardView primCard;
+    private MaterialCardView jhsCard;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_screen);
+        initViews();
+
+        primCard.setOnClickListener(v -> {
+            //TODO: open primary section
+        });
+
+        jhsCard.setOnClickListener(v -> {
+            //TODO: open JHS section
+        });
+
     }
+
+    private void initViews(){
+        primCard = findViewById(R.id.prim_layout);
+        jhsCard = findViewById(R.id.jhs_layout);
+    }
+
 }
